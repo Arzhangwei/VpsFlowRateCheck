18d04eea7c0ad7e813c5be5b93aad2a1d94869c0
Arzhangwei <157704180+Arzhangwei@users.noreply.github.com>
2024-08-29 09:47:15 +0800

    Add files via upload

--- flowRate_warnRemotegithub.py
+++ flowRate_warnRemotegithub.py
@@ -1,65 +1,51 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from DrissionPage import ChromiumPage, ChromiumOptions
+import os
 import asyncio
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium import webdriver
-import requests,os
+import requests
 
 
-
-
-options = ChromeOptions()
-
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--disable-gpu')
-options.add_argument('--remote-debugging-port=9222')
-options.add_argument('--single-process')
-driver = webdriver.Chrome(options=options)
-
-
+# page = ChromiumPage()
+co = ChromiumOptions().headless()
+page = ChromiumPage(co)
 
 
 ss = requests.session()
 PUSHPLUS_TOKEN = os.getenv('PUSHPLUS')
-async def push_wx(conten):
-    result = ss.get(f"https://wxpusher.zjiecode.com/demo/send/custom/{PUSHPLUS_TOKEN}?content={conten}").json()
+
+async def push_wx(content):
+    result = ss.get(f"https://wxpusher.zjiecode.com/demo/send/custom/{PUSHPLUS_TOKEN}?content={content}").json()
     if result['code'] == 1000:
         print(f"账号Wxpusher 通知: 推送成功!")
     else:
         print(f"账号Wxpusher 通知: 推送失败!")
 
-
 async def main():
-    driver.get("https://app.cloudcone.com/login")
-
-    await asyncio.sleep(15)
-
-    driver.find_element(By.XPATH, "/html/body/div[1]/div/div[1]/div/div/div/form/div[1]/input").send_keys(str(os.environ['CC_USERNAME']))
-    driver.find_element(By.XPATH, "//*[@id='password']").send_keys(str(os.environ['CC_PASSWORD']))
-    driver.find_element(By.XPATH, "//*[@id='login-form-btn']").click()
-    await asyncio.sleep(15)
+    # 使用 DrissionPage 打开页面
+    page.get("https://app.cloudcone.com/login")
+    await asyncio.sleep(15)  # 等待页面加载
     
-
-    driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div[2]/div/div[2]/div/table/tbody/tr/td[6]/a").click()
-    await asyncio.sleep(15)
-
+    # 输入用户名和密码并提交表单
+    page.ele("xpath:/html/body/div[1]/div/div[1]/div/div/div/form/div[1]/input").input(str(os.environ['CC_USERNAME']))
+    page.ele("xpath://*[@id='password']").input(str(os.environ['CC_PASSWORD']))
+    page.ele("xpath://*[@id='login-form-btn']").click()
+    await asyncio.sleep(15)  # 等待页面加载 
     
-    totalspace = driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div/div[2]/div/div[1]/div[3]/div[1]/div[1]/small").text
-
-    # 提取已使用的存储空间
-    used_space = totalspace.split(" ")[0]
-
-    # 转换为GB单位
-    used_space_gb = float(used_space)
-
+    # 点击某个按钮以继续操作
+    page.ele('xpath:/html/body/div[2]/div[2]/div/div[2]/div/div[2]/div/table/tbody/tr/td[6]/a').click()
+    await asyncio.sleep(15)  # 等待页面加载
+    
+    # 提取存储空间信息
+    totalspace = page.ele('xpath:/html/body/div[2]/div[2]/div/div/div[2]/div/div[1]/div[3]/div[1]/div[1]/small').text
+    used_space = totalspace.split(" ")[0]  # 提取已使用的存储空间
+    used_space_gb = float(used_space)  # 转换为GB单位
+    
     await push_wx(f'流量使用：{used_space_gb}GB')
     print(f"警告：流量使用{used_space_gb}GB！")
+    
     # 判断是否大于3000GB
-    if used_space_gb > 3000:
-        # 打印告警日志
+    if used_space_gb > 10:
         print("警告：流量使用超过3000GB！")
-        await push_wx('服务器告警：流量使用是超过3000GB！')
+        await push_wx('服务器告警：流量使用超过3000GB！')
+
 # 运行主程序
 asyncio.run(main())
